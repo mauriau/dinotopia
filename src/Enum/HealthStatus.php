42b40010a8e2183a2de27a0261586543fb22a5fe
mauriau <mauriau.pro@proton.me>
2023-03-21 15:24:43 +0100

    Tests pass on GithubService

--- src/Enum/HealthStatus.php
+++ src/Enum/HealthStatus.php
@@ -5,5 +5,5 @@ namespace App\Enum;
 enum HealthStatus: string
 {
     case HEALTHY = 'Healthy';
-    case SICK = 'sick';
+    case SICK = 'Sick';
 }
---
+++ src/Service/GithubService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Service;
+
+use App\Enum\HealthStatus;
+use Symfony\Component\HttpClient\HttpClient;
+
+class GithubService
+{
+    public function getHealthReport(string $dinosaurName): HealthStatus
+    {
+        $client = HttpClient::create();
+
+        $health = HealthStatus::HEALTHY;
+        $response = $client->request(
+            method: 'GET',
+            url: 'https://api.github.com/repos/SymfonyCasts/dino-park/issues'
+        );
+
+        foreach ($response->toArray() as $issue) {
+            if(str_contains($issue['title'], $dinosaurName)) {
+                $health = $this->getDinoStatusFromLabels($issue['labels']);
+            }
+        }
+
+        return $health;
+    }
+
+    private function getDinoStatusFromLabels(array $labels): HealthStatus
+    {
+        $status = null;
+        foreach ($labels as $label) {
+            $label = $label['name'];
+            // We only care about "Status" labels
+            if (!str_starts_with($label, 'Status:')) {
+                continue;
+            }
+            // Remove the "Status:" and whitespace from the label
+            $status = trim(substr($label, strlen('Status:')));
+        }
+
+        return HealthStatus::tryFrom($status);
+    }
+}
